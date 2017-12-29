20dd864f097b72d293184a0e82865347a6131e27
Laurent B <lbnetid+gh@gmail.com>
2017-12-29 11:52:20 +0100

    Introducing Dir patch

--- lib/dir_glob_ignore.rb
+++ lib/dir_glob_ignore.rb
@@ -1,5 +1,6 @@
 require 'dir_glob_ignore/version'
 require 'dir_glob_ignore/ignore_file_list'
+require 'dir_glob_ignore/dir_patch'
 
 module DirGlobIgnore
   # Your code goes here...
---
+++ lib/dir_glob_ignore/dir_patch.rb
@@ -0,0 +1,7 @@
+class Dir
+
+  def self.glob_with_ignore_file(ignore_file, *glob_args)
+
+  end
+
+end
